92dd7af99e28bd37f13bd28c916745afdb5e0ef7
Filip <filip.stybel@justcode.io>
2018-02-04 16:42:09 +0100

    Fix rspec syntax

--- spec/models/statistic_spec.rb
+++ spec/models/statistic_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Statistic, type: :model do
   describe 'validation' do
-    it { should validate_presence_of(:user_id) }
-    it { should validate_presence_of(:agent_id) }
-    it { should validate_presence_of(:incoming_mail_errands) }
-    it { should validate_presence_of(:answered_mail_errands) }
-    it { should validate_presence_of(:total_closed_errands) }
-    it { should validate_presence_of(:handling_time) }
-    it { should validate_presence_of(:lying_untouch_mails) }
+    it { is_expected.to validate_presence_of(:user_id) }
+    it { is_expected.to validate_presence_of(:agent_id) }
+    it { is_expected.to validate_presence_of(:incoming_mail_errands) }
+    it { is_expected.to validate_presence_of(:answered_mail_errands) }
+    it { is_expected.to validate_presence_of(:total_closed_errands) }
+    it { is_expected.to validate_presence_of(:handling_time) }
+    it { is_expected.to validate_presence_of(:lying_untouch_mails) }
   end
 
   describe 'associations' do
-    it { should belong_to(:user) }
+    it { is_expected.to belong_to(:user) }
   end
 
   it "has a valid factory" do
-    create(:statistic).should be_valid
+    expect(create(:statistic)).to be_valid
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validation' do
-    it { should validate_presence_of(:first_name) }
-    it { should validate_presence_of(:last_name) }
-    it { should validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:email) }
   end
 
   describe 'associations' do
-    it { should have_one(:statistic) }
+    it { is_expected.to have_one(:statistic) }
   end
 
   it "has a valid factory" do
-    create(:user).should be_valid
+    expect(create(:user)).to be_valid
   end
 end
